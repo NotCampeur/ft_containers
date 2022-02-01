c3b8ca01c8b40c51be7863873aaa6ebae35a11cc
NotCampeur <lucas.dutriez@gmail.com>
2022-02-01 16:04:04 +0100

    ✨✅ Implemented the whole stack class, the mli's tester valid everything. About to implement unit tests.

--- includes/containers/stack.hpp
+++ includes/containers/stack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   stack.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: notcampeur <notcampeur@student.42.fr>      +#+  +:+       +#+        */
+/*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:23:00 by notcampeur        #+#    #+#             */
-/*   Updated: 2021/10/27 18:24:53 by notcampeur       ###   ########.fr       */
+/*   Updated: 2022/02/01 15:58:22 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,73 @@
 namespace ft
 {
 	template <class T, class Container = ft::vector<T> >
-	class stack;
+	class stack
+	{
+		private:
+			typedef Container							container_type;
+			typedef typename Container::value_type		value_type;
+			typedef typename Container::size_type		size_type;
+			typedef typename Container::reference		reference;
+			typedef typename Container::const_reference	const_reference;
+			
+		protected:
+			Container									c;
+			
+		public:
+			explicit stack( const Container& cont = Container() ) : c(cont) {}
+			stack( const stack& other ) : c(other.c) {}
+			~stack() {}
+
+			stack&	operator=( const stack& other ) { c = other.c; return *this; }
+
+			// Element access
+			reference	top() { return c.back(); }
+			const_reference	top() const { return c.back(); }
+
+			// Capacity
+			bool	empty() const { return c.empty(); }
+			size_type	size() const { return c.size(); }
+
+			// Modifiers
+			void	push( const value_type& val ) { c.push_back(val); }
+			void	pop() { c.pop_back(); }
+	};
+
+	template< class T, class Container >
+	bool	operator==( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return ( lhs.c == rhs.c );
+	}
+
+	template< class T, class Container >
+	bool	operator!=( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return lhs.c != rhs.c;
+	}
+	
+	template< class T, class Container >
+	bool	operator<( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return lhs.c < rhs.c;
+	}
+
+	template< class T, class Container >
+	bool	operator<=( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return lhs.c <= rhs.c;
+	}
+
+	template< class T, class Container >
+	bool	operator>( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return lhs.c > rhs.c;
+	}
+
+	template< class T, class Container >
+	bool	operator>=( const stack<T, Container>& lhs, const stack<T, Container>& rhs )
+	{
+		return lhs.c >= rhs.c;
+	}
 }
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,32 +6,30 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:14:44 by notcampeur        #+#    #+#             */
-/*   Updated: 2022/01/28 18:24:47 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/02/01 16:00:24 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-#ifndef DIY
-	template<typename T>
-	class MutantStack : public ft::stack<T>
-	{
-		public:
-			MutantStack() {}
-			MutantStack(const MutantStack<T>& src) { *this = src; }
-			MutantStack<T>& operator=(const MutantStack<T>& rhs) 
-			{
-				this->c = rhs.c;
-				return *this;
-			}
-			~MutantStack() {}
+template<typename T>
+class MutantStack : public ft::stack<T>
+{
+	public:
+		MutantStack() {}
+		MutantStack(const MutantStack<T>& src) { *this = src; }
+		MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+		{
+			this->c = rhs.c;
+			return *this;
+		}
+		~MutantStack() {}
 
-			typedef typename ft::stack<T>::container_type::iterator iterator;
+		typedef typename ft::stack<T>::container_type::iterator iterator;
 
-			iterator begin() { return this->c.begin(); }
-			iterator end() { return this->c.end(); }
-	};
-#endif
+		iterator begin() { return this->c.begin(); }
+		iterator end() { return this->c.end(); }
+};
 
 long	get_elapsed_time(struct timeval	start_time)
 {
@@ -94,7 +92,7 @@ void	vector_test(void)
 
 void	stack_test(void)
 {
-	#ifndef DIY
+	Logger() << "======================={Stack test}=======================";
 	ft::stack<int> stack_int;
 	ft::stack<Buffer, std::deque<int> > stack_deq_buffer;
 	MutantStack<char> iterable_stack;
@@ -105,7 +103,6 @@ void	stack_test(void)
 	{
 		Logger() << *it;
 	}
-	#endif
 }
 
 void	map_test(void)
@@ -154,7 +151,7 @@ int main(int argc, char** argv)
 	}
 	
 	vector_test();
-	// stack_test();
+	stack_test();
 	// map_test();
 	Logger::quit();
 	return EXIT_SUCCESS;
