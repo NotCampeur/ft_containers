8b05882311540eae93ef0be354dbe2f63eb53ce9
NotCampeur <lucas.dutriez@gmail.com>
2022-02-02 18:49:44 +0100

    ✨💥 Starded to implement map, need to implement bidirectional_iterator.

--- includes/containers/map.hpp
+++ includes/containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: ldutriez <ldutriez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/27 17:23:00 by notcampeur        #+#    #+#             */
-/*   Updated: 2022/02/02 17:03:21 by ldutriez         ###   ########.fr       */
+/*   Updated: 2022/02/02 18:47:14 by ldutriez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,33 @@
 
 namespace ft
 {
-	template < class Key, // map::key_type
-		class T, // map::mapped_type
-		class Compare = std::less<Key>, // map::key_compare
-		class Alloc = std::allocator<std::pair<const Key,T> > > // map::allocator_type
-	class map;
+	template < class Key,
+		class T,
+		class Compare = std::less<Key>,
+		class Alloc = std::allocator<std::pair<const Key,T> > >
+	class map
+	{
+		public:
+			typedef Key key_type;
+			typedef T mapped_type;
+			typedef std::pair<const Key,T> value_type;
+			typedef std::size_t	size_type;
+			typedef std::ptrdiff_t difference_type;
+			typedef Compare key_compare;
+			typedef Alloc allocator_type;
+
+			typedef value_type& reference;
+			typedef const value_type& const_reference;
+
+			typedef typename allocator_type::pointer pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+
+			typedef ft::bidirectional_iterator<value_type> iterator;
+			typedef ft::bidirectional_iterator<const value_type> const_iterator;
+		
+			typedef std::reverse_iterator<iterator> reverse_iterator;
+			typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+	};
 }
 
 #endif
